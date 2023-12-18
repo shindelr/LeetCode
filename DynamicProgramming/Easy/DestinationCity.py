ebce7af6c168f0b9badf23263ed4ec40e7f463bd
shindelr <129821483+shindelr@users.noreply.github.com>
2023-12-18 10:46:42 -0500

    Finished the problem. Did this one iteratively  by memoing the total number of cities in the paths variable then finding the one city that isn't like the others through set-subtraction.

--- DynamicProgramming/Easy/DestinationCity.py
+++ DynamicProgramming/Easy/DestinationCity.py
@@ -3,27 +3,36 @@ from typing import List
 
 # Starter code given below:
 class Solution:
-    def destCity(self, paths: List[List[str]], i: int) -> str:
-        # Memoizations
+    def destCity(self, paths: List[List[str]]) -> str:
+        total_cities = set()
         departures = set()
-        arrivals = set()
 
-        # Base case, if you're at the end of the list, subtract the destination
-        # set from the arrivals, then return whatever is in it.
-        if i > len(paths) - 1:
-            city = arrivals - departures
-            return city.pop()
+        # Add all cities to the total set and all departure cities to theirs.
+        for item in paths:
+            total_cities.add(item[0])
+            total_cities.add(item[1])
+            departures.add(item[0])
 
-
-
-
-
-
-
+        # Subtract the departures from the total number of cities and return
+        # the one that remains.
+        final_set = set(total_cities - departures)
+        return final_set.pop()
 
 # Testing:
 def main():
-    pass
+    solution = Solution()
+
+    paths = [["B", "C"], ["D", "B"], ["C", "A"]]
+    print(solution.destCity(paths))
+
+    paths = [["A", "Z"]]
+    print(solution.destCity(paths))
+
+
+    paths = [["London", "New York"], ["New York", "Lima"],
+             ["Lima", "Sao Paulo"]]
+    print(solution.destCity(paths))
+
 
 if __name__ == "__main__":
     main()
