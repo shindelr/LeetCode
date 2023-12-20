2dfe0277c00f4cbb01e5db8bb81019059669eb24
shindelr <129821483+shindelr@users.noreply.github.com>
2023-12-20 14:45:25 -0800

    Solution to climbing stairs, Top-Down with Memoization. Should be O(n)! I think I'm starting to see a pattern to these algos. First we initialize our basic needs, in this case we intialized a memo and the length of the array to be solved . Second, we lay out our base cases for the recursion. In this context those base cases were the last two indices in the array n=0 and n=1. I'm starting to understand that maybe you establish your base cases then find your way all the way down to those base cases, hit the bottom, then start back out and do all your work on the way outgit add . After the base cases, establish how you can short circuit a normal recursive call using your memo array and if you're unable to short circuit it, then how you can add it to your memo. In this case, we just used the results of our recursive call to add to the memo.

--- DynamicProgramming/Easy/MinCostClimbingStairs.py
+++ DynamicProgramming/Easy/MinCostClimbingStairs.py
@@ -2,28 +2,43 @@ from typing import List
 
 
 class Solution:
+    def __init__(self):
+        self.memo = []  # Memoization array.
+
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
+        self.memo = [0] * n  # Initialize memo array to size of cost array
         return min(self.rec(cost, n-1), self.rec(cost, n-2))
 
     def rec(self, cost, n):
         if n < 0:
             return 0  # This is the empty list, or list of one case?
-        if n == 1 or n == 2:  # Base cases to handle getting to the end.
+        if n == 0 or n == 1:  # Base cases to handle getting to the end.
             return cost[n]
             # Since the parent call is weighing the two ops
             # then we'll be able to see which of the two were more efficient.
 
+        # The memoization here short circuits the recursive calls, so it
+        # doesn't have to brute force its way through every calculation right
+        # back to the beginning of the array.
+        if self.memo[n] != 0:
+            return self.memo[n]
+
+        # The next part works on filling in the memo which is just as important
+        # as the recursive call itself.
+
         # Find the cost of the current index plus the minimum of its next
         # two options.
-        return cost[n] + min(self.rec(cost, n-1), self.rec(cost, n-2))
+        self.memo[n] = cost[n] + min(self.rec(cost, n-1), self.rec(cost, n-2))
+
+        return self.memo[n]
 
 
 def main():
     solution = Solution()
+    cost = [10, 15, 20]
+    cost = [0, 1, 2, 2]
     cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
-    #cost = [10, 15, 20]
-    #cost = [0, 1, 2, 2]
 
     print(solution.minCostClimbingStairs(cost))
 
