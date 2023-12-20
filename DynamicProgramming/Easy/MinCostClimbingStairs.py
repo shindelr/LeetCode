bb8dafb0a99a91d3c4288cd2dffe7f80f7800281
shindelr <129821483+shindelr@users.noreply.github.com>
2023-12-20 10:01:17 -0800

    Set up skeleton code for min cost climbing stairs easy problem.

---
+++ DynamicProgramming/Easy/MinCostClimbingStairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        pass
+
+
+def main():
+    solution = Solution()
+    cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+
+    solution.minCostClimbingStairs(cost)
+
+
+if __name__ == '__main__':
+    main()
