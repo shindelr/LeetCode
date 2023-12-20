e613d112a181e76d8c78930caf4cde5d4707d41c
shindelr <129821483+shindelr@users.noreply.github.com>
2023-12-20 15:22:37 -0800

    Brainstormed for 10 minutes about possible solutions.

---
+++ DynamicProgramming/Easy/DivisorGame.py
@@ -0,0 +1,14 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        pass
+
+
+def main():
+    solution = Solution()
+    n = 2
+    n = 3
+
+    print(solution.divisorGame(n))
+
+if __name__ == '__main__':
+    main()
