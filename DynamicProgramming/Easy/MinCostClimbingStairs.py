463eb9a2fffd2a0a546adb674577fd79a17d9542
shindelr <129821483+shindelr@users.noreply.github.com>
2023-12-20 14:17:58 -0800

    Had to use the solutions to come up with the naive recursive solution. Time to work on the memoization technique. Though I had to look up a solution, the one I came up with myself was fairly close.

--- DynamicProgramming/Easy/MinCostClimbingStairs.py
+++ DynamicProgramming/Easy/MinCostClimbingStairs.py
@@ -1,13 +1,31 @@
+from typing import List
+
+
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        pass
+        n = len(cost)
+        return min(self.rec(cost, n-1), self.rec(cost, n-2))
+
+    def rec(self, cost, n):
+        if n < 0:
+            return 0  # This is the empty list, or list of one case?
+        if n == 1 or n == 2:  # Base cases to handle getting to the end.
+            return cost[n]
+            # Since the parent call is weighing the two ops
+            # then we'll be able to see which of the two were more efficient.
+
+        # Find the cost of the current index plus the minimum of its next
+        # two options.
+        return cost[n] + min(self.rec(cost, n-1), self.rec(cost, n-2))
 
 
 def main():
     solution = Solution()
     cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+    #cost = [10, 15, 20]
+    #cost = [0, 1, 2, 2]
 
-    solution.minCostClimbingStairs(cost)
+    print(solution.minCostClimbingStairs(cost))
 
 
 if __name__ == '__main__':
