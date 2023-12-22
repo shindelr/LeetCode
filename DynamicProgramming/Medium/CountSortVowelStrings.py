6de7e55ec5947e7348fe82a59c458c7488479489
shindelr <129821483+shindelr@users.noreply.github.com>
2023-12-22 12:54:55 -0800

    Wrote out the skeleton code and the base case

--- DynamicProgramming/Medium/CountSortVowelStrings.py
+++ DynamicProgramming/Medium/CountSortVowelStrings.py
@@ -3,7 +3,19 @@ class Solution:
         self.memo = []
 
     def countVowelStrings(self, n: int) -> int:
-        pass
+        if n == 1:
+            return len(self.memo)
 
+
+
+
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    n = 1
+    n = 2
+
+    solution.countVowelStrings(n)
 
 
